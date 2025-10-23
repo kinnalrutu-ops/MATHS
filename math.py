079a8a1622216dc4fff982203961a5b8d0a4f6d1
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 17:02:12 +0530

    save

--- math.py
+++ math.py
@@ -1,6 +1,11 @@
-num1=float(input("enter 1st number")
-num2=float(input("enter 2st number")
+num1=float(input("enter 1st number"))
+num2=float(input("enter 2st number"))
 add=num1+num2
 sub=num1-num2
-print(f"{add}:{num1} + {num2}")
-print(f"{sub}:{num1} + {num2}")
+mul=num1*num2
+div=num1/num2
+print(f"{num1} + {num2} = {add}")
+print(f"{num1} - {num2}= {sub}")
+print(f"{num1} * {num2} = {mul}")
+print(f"{num1} / {num2} = {div}")
+
